66cfd72c28867e5d8e820c4e77eeff7a2bb8cadb
Jeff Lovell <jeff@lovellworks.com>
2021-04-25 20:52:17 -0400

    exercise 4
    Use node to run the foo.js file using node. What does it output?
    Copy and paste the code from foo.js into the node REPL. What does it output?
    Copy and paste the code from foo.js into the Chrome console REPL. What does it output?

---
+++ intro-to-js-programming/exercise4.js
@@ -0,0 +1,14 @@
+// Use node to run the foo.js file using node. What does it output?
+
+bar 
+
+// Copy and paste the code from foo.js into the node REPL. What does it output?
+
+undefined
+bar
+undefined
+
+// Copy and paste the code from foo.js into the Chrome console REPL. What does it output?
+
+bar
+<- "bar"
