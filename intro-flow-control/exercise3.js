1db83c38bb2c6aa4dc42bda3d456874ca432e8d7
Jeff Lovell <jeff@lovellworks.com>
2021-07-01 16:48:18 -0400

    better input handling: 0, non-num types and non-int nums

--- intro-flow-control/exercise3.js
+++ intro-flow-control/exercise3.js
@@ -3,10 +3,15 @@ Let's improve our previous implementation of evenOrOdd. Add a validation check t
 */
 
 function evenOrOdd(num) {
-  if (typeof num === typeof 1) {
-    num % 2 === 0 ? console.log(num + ' is even.') : console.log(num + ' is odd.');
-  } else {
-    console.log(num + ' is a ' + typeof num + ', input must be a number.');
-    return;
+  if (typeof num === typeof 1) { //handle data types other than nums
+    if (num === 0) { //handle 0 and -0
+      console.log(num + ' is neither even nor odd.')
+    } else if (Number.isInteger(num)) { //handle non-int nums
+      num % 2 === 0 ? console.log(num + ' is even.') : console.log(num + ' is odd.');
+    } else {
+      console.log('ERR: ' + num + ' is not an integer, input must be an integer.');
+    }
+  } else { //log the data type and request a number input
+    console.log('ERR: ' + num + ' is a ' + typeof num + ', input must be an number.');
   }
 };
