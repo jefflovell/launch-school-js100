c9e8635b8bba79a2419cedc9444c4e479f55e5eb
Jeff Lovell <jeff@lovellworks.com>
2021-07-10 18:20:29 -0400

    Update exercise2.js

--- intro-arrays/exercise2.js
+++ intro-arrays/exercise2.js
@@ -18,5 +18,27 @@ Copy Code
 */
 
 let myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
-let evens = myArray.filter( num => num % 2 = 0 );
+let evens = myArray.filter( num => num % 2 === 0 );
 evens.forEach(el => console.log(el));
+
+//over engineered...I used the topics being discussed in the chapter.  they used a for loop.  lulz. 
+
+/*
+
+for (let i = 0; i < myArray.length; i += 1) {
+  let value = myArray[i];
+  if (value % 2 === 0) {
+    console.log(value);
+  }
+}
+
+Our approach is straightforward: we iterate over all the elements in the array and check whether each element is even. You can also use forEach and let JavaScript take care of the indexing:
+
+Copy Code
+myArray.forEach(function(value) {
+  if (value % 2 === 0) {
+    console.log(value);
+  }
+});
+
+*/
