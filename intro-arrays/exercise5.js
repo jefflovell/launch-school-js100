d977a8ca6ac251252661d90fceae2ddddc62c4d6
Jeff Lovell <jeff@lovellworks.com>
2021-07-10 20:37:38 -0400

    js100 arrays exercise5.js

---
+++ intro-arrays/exercise5.js
@@ -0,0 +1,35 @@
+/* 
+
+Write a findIntegers function that takes an array argument and returns an array that contains only the integers from the input array. Use the filter method in your function.
+
+Example
+
+Copy Code
+let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
+let integers = findIntegers(things);
+console.log(integers); // => [1, 3, -4]
+
+You can use Number.isInteger(value) to determine whether a numeric value is an integer. It returns true if the value is an integer, false otherwise.
+
+*/
+
+let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false, 9, 17];
+
+// function findIntegers(arr) {
+//   let ints = []
+//   arr.forEach(function(el){
+//     if(Number.isInteger(el)){
+//       ints.push(el);
+//     }
+//   })
+//   return ints;
+// }
+//Oops...said to use filter...
+
+function findIntegers(arr){
+  return arr.filter(el => Number.isInteger(el));
+  
+}
+
+
+
