32cf3310ed5d49039cdc4edb55b65f8afe2b8abd
Jeff Lovell <jeff@lovellworks.com>
2021-07-16 19:29:06 -0400

    js100 objects exercise11.js

---
+++ intro-objects/exercise11.js
@@ -0,0 +1,21 @@
+/*
+Write some code to replace the value 6 in the following object with 606:
+
+Copy Code
+let obj = {
+  foo: { a: "hello", b: "world" },
+  bar: ["example", "mem", null, { xyz: 6 }, 88],
+  qux: [4, 8, 12]
+};
+You don't have to search the object. Just write an assignment that replaces the 6.
+*/
+
+obj.bar[3].xyz = 606;
+
+/*
+Solution
+Solution 1Copy Code
+obj.bar[3].xyz = 606;
+Solution 2Copy Code
+obj["bar"][3]["xyz"] = 606;
+*/
