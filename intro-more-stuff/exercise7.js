abea35d4e0b5df236c3b17bff916be0a7fd14806
Jeff Lovell <jeff@lovellworks.com>
2021-07-19 00:12:37 -0400

    js100 more stuff exercise7.js

---
+++ intro-more-stuff/exercise7.js
@@ -0,0 +1,10 @@
+/*
+What is exception handling and what problem does it solve?
+*/
+
+/* handling exceptional conditions (errors) in a manageable and predictable fashion.  it solves the problem of volatility in input.  it's impossible to know exactly how software will be used which can lead to catastrophic failures and unforeseen consequences.
+*/
+
+/*
+Exception handling is a process that deals with errors in a manageable and predictable manner. It uses the try/catch statement to catch exceptions that the code in the try block raises, and lets the programmer deal with the problem in a way that makes sense and perhaps prevents a catastrophic failure or nasty bug.
+*/
